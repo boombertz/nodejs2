27f1499142ed5246517899af040ea2d2d5c340aa
Hebert Marchesi <bertz@BertzBook-Pro.local>
2022-02-13 22:14:57 -0300

    done

--- src/index.js
+++ src/index.js
@@ -10,19 +10,68 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "user not found" });
+  }
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const totalOfTodos = Object.keys(user.todos).length;
+
+  if (!user.pro && totalOfTodos < 10 || user.pro) {
+    return next();
+  }
+
+  return response.status(403).json({ error: "limit" })
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "user not found" });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "incorret id format" });
+  }
+
+  const findTodoById = user.todos.find(todo => todo.id === id);
+
+  if (!findTodoById) {
+    return response.status(404).json({ error: "todo not found" });
+  }
+  request.user = user;
+  request.todo = findTodoById;
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "user not found" });
+  }
+
+  request.user = user;
+  next();
+
 }
 
 app.post('/users', (request, response) => {
